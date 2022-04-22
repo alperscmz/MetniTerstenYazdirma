2fd69e2432af3157a45a82e1b0ca1005c74157b3
alper saçmaözü <alpersacmaozu@gmail.com>
2022-04-22 16:54:03 +0300

    MetniTerstenYazdirma
    vs c#-Klavyeden girilen metni tersten yazdıran program

---
+++ MetniTerstenYazdirma/MetniTerstenYazdirma/MetniTerstenYazdirma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetniTerstenYazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir metin giriniz!");
+            string metin = Console.ReadLine();
+            Console.WriteLine("Metninizin ters hali:");
+            for (int i = metin.Length - 1; i >= 0; i--)
+            {
+                Console.Write(metin[i]);
+               
+            }
+            Console.Read();
+        }
+    }
+}
